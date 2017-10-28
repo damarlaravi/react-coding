8021f22f7eff28df689019706e62937a3ec4b685
rdemarla <damarlaravi@gmail.com>
2017-10-28 11:19:57 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+//import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <div> Hi!</div>
+};
+
+ReactDOM.render(<App />, document.querySelector('#app'));
